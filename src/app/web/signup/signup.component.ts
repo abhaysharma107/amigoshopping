3d0f0215572fa2e4561a668acb62a183cfcbaf58
Abhay <abhay.shar107@gmail.com>
2021-12-24 12:56:13 +0530

    front-end validation for signup form

--- src/app/web/signup/signup.component.ts
+++ src/app/web/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +7,41 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  constructor() { }
-
+  signup:any
+  constructor(private fb:FormBuilder) { }
+ 
   ngOnInit(): void {
+    this.signup = this.fb.group({
+      firstname:['',[
+        Validators.required
+      ]],
+      emailform: ['',[
+        Validators.required,Validators.email
+      ]],
+      address:['',[
+         Validators.required
+      ]],
+      city:['',[
+         Validators.required
+      ]],
+      state:['',[
+         Validators.required
+      ]],
+      pincode:['',[
+         Validators.required
+      ]],
+    })
   }
+
+  onSubmit(){
+    console.log(this.signup.value);
+    
+  }
+  get firstname() {return this.signup.get('firstname');}
+  get emailform() {return this.signup.get('emailform');}
+  get address() {return this.signup.get('address');}
+  get city() {return this.signup.get('city');}
+  get state() {return this.signup.get('state');}
+  get pincode() {return this.signup.get('pincode');}
 
 }
