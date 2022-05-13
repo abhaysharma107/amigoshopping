cbce6f2845d32382c303a6414ee879928a14db69
Abhay <abhay.shar107@gmail.com>
2022-05-13 22:46:45 +0530

    backend server name change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // url:'http://localhost:8080/',
-  url:"https://evening-forest-55820.herokuapp.com"
+  url:"https://backed-for-all-apps.herokuapp.com"
 };
