6e05774f27b1875199ace227adbed8ea170528a9
Abhay <abhay.shar107@gmail.com>
2022-06-02 14:04:21 +0530

    added payment getway

--- src/app/_service/products.service.ts
+++ src/app/_service/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Product } from '../_interface/product';
 import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment'
+
+function _window() : any {
+  // return the global native browser window object
+  return window;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +21,15 @@ export class ProductsService {
   womenswear = environment.url + '/products/womensclothing'
   jewelery = environment.url + '/products/jewelery'
   details = environment.url + '/products/details'
+  orderId = environment.url + '/products/create/orderId'
+  signature = environment.url + '/signature'
 
   constructor(private http: HttpClient) { }
+
+  get nativeWindow() : any {
+    return _window();
+ }
+
   getProducts():Observable<Product[]>{
     return this.http.get<Product[]>(this.products)
     .pipe(
@@ -66,4 +78,16 @@ export class ProductsService {
   getProductDetails(productId: any){
     return this.http.post<Product[]>(this.details, {productId:productId})
   }
+
+  getOrderId(amount:number){
+    return this.http.post<any>(this.orderId, {price:amount})
+  }
+
+  sendSignature(razorpay_order_id:any, razorpay_payment_id:any, razorpay_signature:any, productId:any){
+    return this.http.post<any>(this.signature, {razorpay_order_id:razorpay_order_id, razorpay_payment_id:razorpay_payment_id, razorpay_signature:razorpay_signature, productId:productId},  {
+      headers: new HttpHeaders({
+        'authorization': 'Bearer '+ localStorage.getItem('token'),
+      })
+  })
+}
 }
--- src/app/web/product-listing-page/mens-wear/mens-wear.component.ts
+++ src/app/web/product-listing-page/mens-wear/mens-wear.component.ts
@@ -14,6 +14,7 @@ export class MensWearComponent implements OnInit {
   product:any=[];
   isLoggedIn=false;
   productNumberReturn:Array<any> = [];
+  rzp1: any;
   constructor(private productsService: ProductsService,
     private cartService: CartService,
     private checkLoginService: CheckloginService,
@@ -61,13 +62,53 @@ export class MensWearComponent implements OnInit {
       this.router.navigate(['/login'])
     }
   }
-  buyNow(data:any){
+  buyNow(data: any) {
     if (this.isLoggedIn == true) {
-      console.log("Ok You can buy it now");
-    }
-    else {
-      this.toster.warning("Please Login First")
-      this.router.navigate(['/login'])
+      this.productsService.getOrderId(data.price).subscribe(orderId => {
+        console.log(orderId);
+        let options = {
+          key: "rzp_test_lpypW4eeQg5R6n", 
+          amount: data.price * 100, 
+          currency: "INR",
+          name: "AmigoShopping",
+          description: "Test Transaction",
+          image: "",
+          order_id: orderId.orderId, 
+          handler: (response:any) =>{
+            this.productsService.sendSignature(response.razorpay_payment_id,response.razorpay_order_id,response.razorpay_signature, data._id).subscribe(data =>{
+              if (data.status == true) {
+                alert('Your Order Has Been Placed')
+              }
+            })
+        },
+          prefill: {
+              name: "Gaurav Kumar",
+              email: "gaurav.kumar@example.com",
+              contact: "9999999999"
+          },
+          notes: {
+              address: "Razorpay Corporate Office"
+          },
+          theme: {
+              color: "#3399cc"
+          }
+      }
+        this.rzp1 = new this.productsService.nativeWindow.Razorpay(options);
+        this.rzp1.open();
+        this.rzp1.on('payment.failed', (response: any) => {
+          // alert(response.error.code);
+          alert(response.error.description);
+          // alert(response.error.source);
+          // alert(response.error.step);
+          // alert(response.error.reason);
+          // alert(response.error.metadata.order_id);
+          // alert(response.error.metadata.payment_id);
+        });
+      })
+      
+    } else {
+      this.toster.warning('Please Login First');
+      this.router.navigate(['/login']);
     }
   }
 }
